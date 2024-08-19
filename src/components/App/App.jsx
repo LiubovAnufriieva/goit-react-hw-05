cf632659158c249ea1d2ce1d2092ea2203b9a159
Lyuzika <lyuzika@gmail.com>
2024-08-19 17:58:55 +0300

    some changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,23 +2,20 @@ import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import css from "./App.module.css";
+import Loader from "../Loader/Loader";
 
 const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
 const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
-const MovieDetailsPage = lazy(() =>
-  import("../../pages/MovieDetailsPage/MovieDetailsPage")
-);
+const MovieDetailsPage = lazy(() => import("../../pages/MovieDetailsPage/MovieDetailsPage"));
 const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
-const MovieReviews = lazy(() =>
-  import("../MovieReviews/MovieReviews")
-);
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
 const NotFoundPage = lazy(() => import("../../pages/NotFoundPage/NotFoundPage"));
 
 const App = () => {
   return (
     <div className={css.container}>
       <Navigation />
-      <Suspense fallback={<div>Loading page...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
